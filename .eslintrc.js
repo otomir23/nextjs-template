0574fa4f37301f6378b4e73377b0c8174a90fa13
Damir Modyarov <otomir@yandex.ru>
2023-11-22 15:31:25 +0300

    chore: Setup eslint stylistic

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+const stylistic = require("@stylistic/eslint-plugin")
+
+const customized = stylistic.configs.customize({
+    indent: 4,
+    quotes: "double",
+    semi: false,
+    jsx: true,
+})
+
+module.exports = {
+    extends: "next/core-web-vitals",
+    plugins: [
+        "@stylistic",
+    ],
+    rules: {
+        ...customized.rules,
+        "@stylistic/jsx-one-expression-per-line": "off",
+        "@stylistic/max-len": ["warn", 120],
+        "@stylistic/no-trailing-spaces": ["error", { skipBlankLines: true }],
+        "@stylistic/member-delimiter-style": ["error", {
+            multiline: {
+                delimiter: "comma",
+                requireLast: true,
+            },
+            singleline: {
+                delimiter: "comma",
+                requireLast: false,
+            },
+        }],
+    },
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,15 @@
-import type {Metadata} from "next";
-import type {ReactNode} from "react";
-import {Inter} from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import type { ReactNode } from "react"
+import { Inter } from "next/font/google"
+import "./globals.css"
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-    title: 'Webpage',
+    title: "Webpage",
 }
 
-export default function RootLayout({children}: {
-    children: ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html lang="en">
             <body className={inter.className}>{children}</body>
